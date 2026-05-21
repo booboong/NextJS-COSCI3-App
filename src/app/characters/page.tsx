656735e6473f11623bcc552106fc1b4aeed887c4
Boong <boongwwa150@gmail.com>
2026-05-21 10:45:24 +0700

    update-CharactersPage

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -2,9 +2,15 @@ import FeaturesCharacters from "@/components/features-characters";
 import Link from "next/link";
 
 // http://localhost:3000/characters
-export default function CharactersPage() {
+export default async function CharactersPage() {
+    const response = await fetch("https://api.codingthailand.com/api/course");
+    const courseResponse = await response.json();
+
   return (
     <div>
+      {
+        JSON.stringify(courseResponse)
+      }
       <FeaturesCharacters/>
       {/* Characters Page
       <p/>
